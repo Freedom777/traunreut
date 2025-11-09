22eb6f54e501ced1f799ddf66fb79f9a5e21111b
Oleg Freedom <olegfreedom777@gmail.com>
2025-11-09 23:14:56 +0100

    Cron changed without console.php changes

--- routes/console.php
+++ routes/console.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 /*
 crontab -e
-* * * * * cd /var/www/traunreut && php artisan schedule:run >> /dev/null 2>&1
+# Только crontab
+30 23 28-31 * * [ $(date -d tomorrow +\%d) -eq 1 ] && cd /var/www/traunreut && php artisan parse:site --site=all >> /var/www/traunreut/storage/logs/parse.log 2>&1
+50 23 28-31 * * [ $(date -d tomorrow +\%d) -eq 1 ] && cd /var/www/traunreut && php artisan translate:words >> /var/www/traunreut/storage/logs/translate.log 2>&1
 */
-Schedule::command('parse:site', ['--site' => 'all'])->monthlyOn(1, '00:01')->appendOutputTo(storage_path('logs/parse.log'));;
-Schedule::command('translate:words')->monthlyOn(1, '00:20')->appendOutputTo(storage_path('logs/translate.log'));;
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
