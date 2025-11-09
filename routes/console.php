7dea4c38cd1de267248d4b38301ab8a5407b7dea
Oleg Freedom <olegfreedom777@gmail.com>
2025-11-09 18:24:31 +0100

    Cron added

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+/*
+crontab -e
+* * * * * cd /var/www/traunreut && php artisan schedule:run >> /dev/null 2>&1
+*/
+Schedule::command('parse:site', ['--site' => 'all'])->monthlyOn(1, '00:01')->appendOutputTo(storage_path('logs/parse.log'));;
+Schedule::command('translate:words')->monthlyOn(1, '00:20')->appendOutputTo(storage_path('logs/translate.log'));;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
