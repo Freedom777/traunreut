338b320aaa6aa60ce0d0a20975d46e2f94738a57
Oleg Freedom <olegfreedom777@gmail.com>
2026-04-21 21:05:29 +0200

    Debug Telegram requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Доверять Nginx proxy
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'log.telegram' => \App\Http\Middleware\LogTelegramRequests::class,
         ]);
