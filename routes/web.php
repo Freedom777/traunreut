61ad229b12e4c5b1a94aee5a464b93d72b92d8d2
Oleg Freedom <olegfreedom777@gmail.com>
2026-04-22 07:49:36 +0200

    Debug Telegram requests

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 /*Route::post('/telegraph/{token}/webhook', [TelegramWebhookHandler::class, 'handle'])
     ->name('telegraph.webhook')
     ->where('token', '.*');*/
+Route::post('/test-nocsrf', function() {
+    return 'OK';
+})->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class);
